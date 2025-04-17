a729bbcb3efa4dedf9facbaa1d3228d1c0a401d8
catcatsang <rksel0712@gmail.com>
2025-04-17 09:47:03 +0900

    init

---
+++ src/main/java/com/app/bookJeog/BookjeogApplication.java
@@ -0,0 +1,13 @@
+package com.app.bookJeog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookjeogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookjeogApplication.class, args);
+    }
+
+}
