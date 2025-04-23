0146da45ece7da9c1b697592c58e01eec04ad380
KSG-gbtest <ksbacteria@gmail.com>
2025-04-23 14:18:37 +0900

    공지 작성 & 수정까지 완료

--- src/main/java/com/app/bookJeog/repository/NoticeDAO.java
+++ src/main/java/com/app/bookJeog/repository/NoticeDAO.java
@@ -1,10 +1,36 @@
 package com.app.bookJeog.repository;
 
+import com.app.bookJeog.domain.dto.Pagination;
+import com.app.bookJeog.domain.vo.NoticeVO;
+import com.app.bookJeog.mapper.NoticeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class NoticeDAO {
+    private final NoticeMapper noticeMapper;
+
+    public List<NoticeVO> findAllNotice(Pagination pagination){
+        return noticeMapper.selectAllNotice(pagination);
+    }
+
+    public int countAllNotice(Pagination pagination){
+        return noticeMapper.countAllNotice(pagination);
+    }
+
+    public NoticeVO selectNoticeById(Long id){
+        return noticeMapper.selectNoticeById(id);
+    }
+
+    public void updateNotice(NoticeVO noticeVO){
+        noticeMapper.updateNotice(noticeVO);
+    }
+
+    public void insertNotice(NoticeVO noticeVO){
+        noticeMapper.insertNotice(noticeVO);
+    }
 
 }
