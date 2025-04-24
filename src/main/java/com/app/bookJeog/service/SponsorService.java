0c0d6d49548d86b6c6e2e45395ea8cfcccfc47e1
catcatsang <rksel0712@gmail.com>
2025-04-24 14:11:16 +0900

    로그인 수정 완료

--- src/main/java/com/app/bookJeog/service/SponsorService.java
+++ src/main/java/com/app/bookJeog/service/SponsorService.java
@@ -1,8 +1,6 @@
 package com.app.bookJeog.service;
 
-import com.app.bookJeog.domain.dto.PersonalMemberDTO;
-import com.app.bookJeog.domain.dto.SponsormemberDTO;
-import com.app.bookJeog.domain.vo.PersonalMemberVO;
+import com.app.bookJeog.domain.dto.SponsorMemberDTO;
 import com.app.bookJeog.domain.vo.SponsorMemberVO;
 
 import java.util.Optional;
@@ -10,22 +8,28 @@ import java.util.Optional;
 public interface SponsorService {
 
     // 단체 로그인
-    public Optional<SponsorMemberVO> loginSponsorMember (SponsormemberDTO sponsorMemberDTO);
+    public Optional<SponsorMemberVO> loginSponsorMember (SponsorMemberDTO sponsorMemberDTO);
 
-    public default SponsorMemberVO toSponsorMemberVO (SponsormemberDTO sponsorMemberDTO) {
+    public default SponsorMemberVO toSponsorMemberVO (SponsorMemberDTO sponsorMemberDTO) {
         return  SponsorMemberVO.builder()
+                .id(sponsorMemberDTO.getId())
                 .sponsorId(sponsorMemberDTO.getSponsorId())
                 .sponsorPassword(sponsorMemberDTO.getSponsorPassword())
                 .sponsorEmail(sponsorMemberDTO.getSponsorEmail())
+                .sponsorName(sponsorMemberDTO.getSponsorName())
+                .sponsorPhone(sponsorMemberDTO.getSponsorPhone())
+                .sponsorMainAddress(sponsorMemberDTO.getSponsorMainAddress())
+                .sponsorMemberStatus(sponsorMemberDTO.getSponsorMemberStatus())
+                .sponsorSubAddress(sponsorMemberDTO.getSponsorSubAddress())
                 .build();
     }
 
 
     // 비밀번호 변경
-    public void changePassword(SponsormemberDTO sponsorMemberDTO, String newPasswd);
+    public void changePassword(SponsorMemberDTO sponsorMemberDTO, String newPasswd);
 
 
 
     // 이메일 중복검사
-    public Optional<SponsorMemberVO> selectEmailForPassword(SponsormemberDTO sponsorMemberDTO);
+    public Optional<SponsorMemberVO> selectEmailForPassword(SponsorMemberDTO sponsorMemberDTO);
 }
