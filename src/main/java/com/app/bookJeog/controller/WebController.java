231bde723ffe7e1ce08182c175e8385d888d88e8
catcatsang <rksel0712@gmail.com>
2025-04-24 15:57:40 +0900

    로그인 수정 2.0

--- src/main/java/com/app/bookJeog/controller/WebController.java
+++ src/main/java/com/app/bookJeog/controller/WebController.java
@@ -2,6 +2,7 @@ package com.app.bookJeog.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,7 +11,8 @@ public class WebController {
 
     // 메인으로 이동
     @GetMapping("main/main")
-    public String goToMain() {
+    public String goToMain(Model model) {
+
         return "main/main";
     }
 }
