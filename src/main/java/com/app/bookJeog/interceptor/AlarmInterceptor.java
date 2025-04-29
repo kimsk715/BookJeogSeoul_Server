5aeb3aa72d1b2789a0d6facf4f50978e0c13f1d7
catcatsang <rksel0712@gmail.com>
2025-04-29 17:15:20 +0900

    알람 시작 , 로그인 또 수정

--- src/main/java/com/app/bookJeog/interceptor/AlarmInterceptor.java
+++ src/main/java/com/app/bookJeog/interceptor/AlarmInterceptor.java
@@ -5,16 +5,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @RequiredArgsConstructor
 public class AlarmInterceptor implements HandlerInterceptor {
 
 
-
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        request.setAttribute("test", "test1");
-        return true;
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
     }
+
+//    @Override
+//    public boolean postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        request.setAttribute("test", "test1");
+//        return true;
+//    }
 }
