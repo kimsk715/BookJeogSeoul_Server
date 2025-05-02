a3acde358c4b4baf0f878780e48b5cfbd3f5e03d
catcatsang <rksel0712@gmail.com>
2025-05-02 14:55:57 +0900

    알람 90퍼

--- src/main/java/com/app/bookJeog/interceptor/AlarmInterceptor.java
+++ src/main/java/com/app/bookJeog/interceptor/AlarmInterceptor.java
@@ -1,25 +1,37 @@
 package com.app.bookJeog.interceptor;
 
+import com.app.bookJeog.domain.dto.PersonalMemberDTO;
+import com.app.bookJeog.domain.vo.MemberVO;
+import com.app.bookJeog.domain.vo.PersonalMemberVO;
+import com.app.bookJeog.service.AlarmService;
+import com.app.bookJeog.service.AlarmServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class AlarmInterceptor implements HandlerInterceptor {
-
+    private final AlarmService alarmService;
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HttpSession session = request.getSession(false);
+        if (session != null && modelAndView != null) {
+           PersonalMemberDTO personalMemberDTO = (PersonalMemberDTO) session.getAttribute("member");
 
-    }
 
-//    @Override
-//    public boolean postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        request.setAttribute("test", "test1");
-//        return true;
-//    }
+                int unreadCount = alarmService.unreadAlarmCount(personalMemberDTO.getId());
+                log.info("unread count: {}", unreadCount);
+
+                modelAndView.addObject("unreadAlarmCount", unreadCount);
+
+        }
+   }
 }
--- src/main/java/com/app/bookJeog/mapper/AlarmMapper.java
+++ src/main/java/com/app/bookJeog/mapper/AlarmMapper.java
@@ -1,11 +1,50 @@
 package com.app.bookJeog.mapper;
 
+import com.app.bookJeog.domain.dto.MyAlarmDTO;
+import com.app.bookJeog.domain.enumeration.AlarmStatus;
+import com.app.bookJeog.domain.vo.*;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface AlarmMapper {
 
-    //댓글 알람
-    public void insertCommentMention();
+
+    //알람 슈퍼키
+    public void insertAlarm(AlarmVO alarmVO);
+
+
+    // 내가 작성한 게시글 등록
+    public void insertCommentAlarm(CommentAlarmVO commentAlarmVO);
+
+
+    // 누군가 맨션 알람 등록
+    public void insertMentionAlarm(CommentMentionAlarmVO commentMentionAlarmVO);
+
+
+    // 팔로잉한 사람의 게시글 알림
+    public void insertPostAlarm(PostAlarmVO postAlarmVO);
+
+
+    // 팔로잉 한 사람
+    public void insertFollowAlarm(FollowAlarmVO followAlarmVO);
+
+
+    // 내가 작성한 게시글 답글 조회
+    public Optional<MyAlarmDTO> selectMyPostOnlyComment();
+
+
+    // 안읽은 알람 갯수
+    public int selectAlarmCount(Long memberId);
+
+
+    // 읽은 알람
+    public void updateAlarmRead(Long receiverId);
+
+
+
+
+
 
 }
